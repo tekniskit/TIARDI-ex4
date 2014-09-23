0c4c22c184b7de6c089d88accacbcd129c435234
Rasmus Reimer <rasmus.reimer@gmail.com>
2014-09-23 10:53:03 +0200

    fixed PatientInformationService

--- server/PatientInformationService.cpp
+++ server/PatientInformationService.cpp
@@ -1,7 +1,16 @@
 #include "PatientInformationService.h"
+#include <fstream>
 
-Patient PatientService::getPatient(string cprNumber) {
-	search(cprNumber);
+Patient PatientInformationService::getPatient(CprNumber cprNumber) {
+	ifstream infile("patiens.csv");
+
+	string line;
+	while (getline(infile, line))
+	{
+		/*if (line.find() == string::npos) continue;
+
+			cprNumber_ = cprNumber;*/
+	}
+
 	return Patient();
 }
-
