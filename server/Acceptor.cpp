217b862b8a5c13ccda7ea8d694d23db7e07a646f
Martin Jensen <martin2610@gmail.com>
2014-09-26 12:17:49 +0200

    Acceptor implementation.

--- server/Acceptor.cpp
+++ server/Acceptor.cpp
@@ -4,15 +4,21 @@ Acceptor::Acceptor(){
 }
 
 Acceptor::Acceptor(int port, IReactor* reactor){
-
+	INET_Addr addr(port, 0x7F000001);
+	peer_acceptor_ = new SOCK_Acceptor(addr);
+	reactor->registerHandler(this, 6); // 6 is the event type for the acceptor (in Demux en exercise 4)
 }
 
 Acceptor::~Acceptor(){
+	delete peer_acceptor_;
 }
 
 void Acceptor::handleEvent(Handle* handle){
-
+	accept();
 }
 void Acceptor::accept(){
-
+	SOCK_Stream* stream = new SOCK_Stream();
+	peer_acceptor_->accept(*stream);
+	socketList.push_back(stream);
+	std::cout << "Client connected" << std::endl;
 }
--- server/Acceptor.h
+++ server/Acceptor.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "IAcceptor.h"
 #include "IReactor.h"
+#include "SOCK_Acceptor.h"
+#include "SOCK_Stream.h"
+#include <list>
+#include <iostream>
+
 class Acceptor : public IAcceptor
 {
 public:
@@ -9,4 +14,8 @@ public:
 	~Acceptor();
 	void handleEvent(Handle* handle);
 	void accept();
+private:
+	std::list<SOCK_Stream*> socketList;
+	SOCK_Acceptor* peer_acceptor_;
+	
 };
