147381d758fff26f2171fd0ccf5690b777a60d3c
Rasmus Reimer <rasmus.reimer@gmail.com>
2014-09-26 11:29:19 +0200

    Fixed bugs in PatientInformationService

--- server/PatientInformationService.cpp
+++ server/PatientInformationService.cpp
@@ -1,16 +1,34 @@
 #include "PatientInformationService.h"
 #include <fstream>
 
+void Tokenize(const string& str, vector<string>& tokens, const string& delimiters = " ") {
+	string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+	string::size_type pos = str.find_first_of(delimiters, lastPos);
+	
+	while (string::npos != pos || string::npos != lastPos)
+	{
+		tokens.push_back(str.substr(lastPos, pos - lastPos));
+		lastPos = str.find_first_not_of(delimiters, pos);
+		pos = str.find_first_of(delimiters, lastPos);
+	}
+}
+
 Patient PatientInformationService::getPatient(CprNumber cprNumber) {
-	ifstream infile("patiens.csv");
+	ifstream infile("patients.txt");
 
 	string line;
 	while (getline(infile, line))
 	{
-		/*if (line.find() == string::npos) continue;
+		if (line.find(cprNumber.getCprNumber()) == string::npos) continue;
 
-			cprNumber_ = cprNumber;*/
+		vector<string> raw;
+
+		Tokenize(line, raw, ";");
+
+		CprNumber cpr(raw[0]);
+		string name = raw[1];
+		string address = raw[2];
+		
+		return Patient(cpr, name, address);
 	}
-
-	return Patient();
 }
